65fbd7384e4a926cfce6f10aefdc53ba156e8505
anuu0916 <youjin1939@gmail.com>
2021-08-19 21:07:56 +0900

    11722 가장 긴 감소하는 부분 수열 - 7th study (DP)

--- baekjoon/11722.py
+++ baekjoon/11722.py
@@ -2,11 +2,9 @@
 import sys
 n = int(input())
 a = list(map(int, sys.stdin.readline().split()))
-d = [0] * n
-d[0] = 1
+d = [1] * n
 
 for i in range(1, n):
-    d[i] = 1  # 1로 초기화
     for j in range(i):  # 앞에서부터 현재 수보다 큰 수를 찾음
         if a[i] < a[j]:
             d[i] = max(d[i], d[j] + 1)  # 감소하는 길이 중 가장 큰 값
