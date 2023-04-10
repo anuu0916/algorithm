af2dd0b5945d80ebf0e0fcda153affbcb357aa4b
Youjin Ahn <56395764+anuu0916@users.noreply.github.com>
2023-04-10 16:10:13 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 112 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int cnt = 0;
+		String str = "";
+		while((str = br.readLine()) != null) {
+			cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
